a951ea21d1c16b4f75f2041b759dde8339aaabbd
jake_ryan <jaker@getfluid.com>
2016-02-08 16:32:18 -0700

    header text and styles

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).foundation();
 $(document).ready(function() {
 
   function shadow(){
-    $('#hero h1').delay(9000).addClass('shadow');
+    $('#hero h1').addClass('shadow');
   }
   setTimeout(shadow, 900);
 
