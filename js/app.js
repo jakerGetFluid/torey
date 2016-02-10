b32570307300a7d26e093b4fe81abda5526f3d49
jake_ryan <jaker@getfluid.com>
2016-02-10 08:56:27 -0700

    add type animation and start bio animation

--- js/app.js
+++ js/app.js
@@ -6,12 +6,20 @@ $(document).ready(function() {
   }
   setTimeout(shadow, 900);
 
-  $('#bio .row.pic').hover(function() {
+
+  //bio section, on hover, image swap for color
+  $('#bio .row > .column').hover(function() {
     $('img.bw', this).hide();
     $('img.color', this).show();
   }, function() {
     $('img.color', this).hide();
     $('img.bw', this).show();
+  });
+
+  //bio seciton, on click, fly away not-clicked items, display hidden content
+  $('#bio .row > .column').click(function() {
+    //select copy of selected column
+    $(this).next('.column').fadeIn('400');
   });
 
 });
--- js/min/app-min.js
+++ js/min/app-min.js
@@ -1,2 +1,2 @@
-$(document).foundation(),$(document).ready(function(){function o(){$("#hero h1").addClass("shadow")}setTimeout(o,900),$("#bio .row.pic").hover(function(){$("img.bw",this).hide(),$("img.color",this).show()},function(){$("img.color",this).hide(),$("img.bw",this).show()})});
+$(document).foundation(),$(document).ready(function(){function o(){$("#hero h1").addClass("shadow")}setTimeout(o,900),$("#bio .row > .column").hover(function(){$("img.bw",this).hide(),$("img.color",this).show()},function(){$("img.color",this).hide(),$("img.bw",this).show()}),$("#bio .row > .column").click(function(){$(this).next(".column").fadeIn("400")})});
 //# sourceMappingURL=./app-min.js.map
